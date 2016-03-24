8d9239ddb1fc40bd6e7ce6d9c63790d5d5e8bd19
Nandita Banerjee <nandita.skywalker@gmail.com>
2016-03-24 23:10:54 +0530

    solved problem1

--- python problem1.py
+++ python problem1.py
@@ -3,8 +3,8 @@ IntN = int(input("Enter an integer number:"))
 FloatN = float(input("Enter a floating number:"))
 print ("Sum of %i and %.2f is: %.2f" % (IntN, FloatN, IntN+FloatN))
 #Program to find the sum, difference, multiplication and division of two numbers.
-num1= int(input("Enter first number:"))
-num2= int(input("Enter second number:"))
+Num1= int(input("Enter first number:"))
+Num2= int(input("Enter second number:"))
 Sum= Num1 + Num2
 diff= Num1 - Num2
 mult= Num1 * Num2
